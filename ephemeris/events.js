b7e12bc99a6f6ad029d2191589b52b8324658766
Il_aime0_0 <ilhempsnm@gmail.com>
2026-08-30 16:38:07 -0400

    Refactor strangeEvents object for clarity

--- ephemeris/events.js
+++ ephemeris/events.js
@@ -3,6 +3,9 @@
 
     Add entries using:
     "YYYY-MM-DD": "text"
+
+    You can also use an array for multiple
+    events on the same date.
 */
 
 const strangeEvents = {
@@ -13,24 +16,21 @@ const strangeEvents = {
     "2026-08-17":
         "This date has been recorded twice.",
 
+    "2026-08-29": [
+        "Simbiosis in Québec.",
+        "First day of this year. Happy New Year!"
+    ],
+
     "2026-09-03":
         "You already missed this.",
 
     "2026-09-17":
-        "This date should not exist.",
+        "In between.",
 
     "2026-10-31":
         "Something is waiting here.",
 
     "2026-12-31":
-        "LAST RECORDED DATE.", 
- 
-   "2026-09-17":
-        "In between.", 
-   
-    "2026-08-29": [
-        "Simbiosis in Québec."
-        
-        "First day of this year. Happy New Year!"
-],
+        "LAST RECORDED DATE."
+
 };
